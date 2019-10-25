a2632f6bb162ae3cab9c0bd8af040ae5fa49e7db
Evan Witt <evanawitt@gmail.com>
2019-10-24 18:07:17 -0700

    Started on building my neural network.

---
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/starterc.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Introduction file for Udemy's Deep Learning course.
+
+Library Review:
+    
+    Theano - An open source efficient numeric computations library, based on numpy syntax. Useful, in that it can run on your GPU.
+    
+    TensorFlow - Used professionally (and in academia) to build neural networks from scratch.
+    
+    Keras - Based on Theano and TensorFlow. Simplifies the building of deep learning models.
+
+"""
+
+
+
+print ("Hello world!")
